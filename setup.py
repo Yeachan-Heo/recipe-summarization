bebe491cf697080387d6d3c451aa3c75048047d7
Yeachan-Heo <yeachanheo@Yeachans-MacBook-Pro.local>
2020-09-30 15:04:07 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='recipe-summarization',
+    version='0.0.0',
+    packages=['recipe_summarization'],
+    url='',
+    license='',
+    author='Yeachan-Heo',
+    author_email='',
+    description=''
+)
